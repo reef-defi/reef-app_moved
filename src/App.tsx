9f0d9c775c865209aa57395e0fe0c5de8adc1b27
Žiga Franko <franko.ziga@gmail.com>
2021-09-27 14:29:20 +0200

    router dom and redux store included in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
+import { Provider as StoreProvider } from 'react-redux';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
 import Sidebar from './common/Sidebar';
 import Nav from './common/Nav';
 import ContentRouter from './pages/ContentRouter';
+import { store } from './store';
 
 const App = (): JSX.Element => (
-  <div className="App d-flex w-100 h-100">
-    <Sidebar />
-    <div className="w-100">
-      <Nav />
-      <ContentRouter />
-    </div>
-  </div>
+  <Router>
+    <StoreProvider store={store}>
+      <div className="App d-flex w-100 h-100">
+        <Sidebar />
+        <div className="w-100">
+          <Nav />
+          <ContentRouter />
+        </div>
+      </div>
+    </StoreProvider>
+  </Router>
 );
 
 export default App;
