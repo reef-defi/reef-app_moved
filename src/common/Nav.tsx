65c0bce4c932fd50b44ff93fdf6da088413eb18b
Žiga Franko <franko.ziga@gmail.com>
2021-09-28 16:09:39 +0200

    sidebar style update and links added

--- src/common/Nav.tsx
+++ src/common/Nav.tsx
@@ -3,7 +3,7 @@ import { Components } from '@reef-defi/react-lib';
 import './Nav.css';
 
 const Nav = (): JSX.Element => {
-  const signersExist = true;
+  const signersExist = false;
 
   return (
     <nav className="navigation px-3 py-2 d-flex justify-content-end">
