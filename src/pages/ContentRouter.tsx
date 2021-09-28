ef3301af3f9dfd212449882caf9605e5e4462776
Žiga Franko <franko.ziga@gmail.com>
2021-09-28 16:09:21 +0200

    default page components with routing integrated

--- src/pages/ContentRouter.tsx
+++ src/pages/ContentRouter.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { ADD_LIQUIDITY_URL, CREATE_ERC20_TOKEN_URL, DASHBOARD_URL, REMOVE_LIQUIDITY_URL, SETTINGS_URL, SWAP_URL } from '../urls';
 
 const ContentRouter = (): JSX.Element => (
   <div className="content">
-    Content router
+    <Switch>
+      <Route exact path={SWAP_URL} render={() => (<div/>)} />
+      <Route exact path={SETTINGS_URL} render={() => (<div/>)} />
+      <Route exact path={DASHBOARD_URL} render={() => (<div/>)} />
+      <Route exact path={ADD_LIQUIDITY_URL} render={() => (<div/>)} />
+      <Route exact path={REMOVE_LIQUIDITY_URL} render={() => (<div/>)} />
+      <Route exact path={CREATE_ERC20_TOKEN_URL} render={() => (<div/>)} />
+      <Route path="/" render={() => (<Redirect to={DASHBOARD_URL} />)} />
+    </Switch>
   </div>
 );
 
--- src/urls/index.ts
+++ src/urls/index.ts
@@ -1,4 +1,5 @@
 export const SWAP_URL = "/swap";
+export const SETTINGS_URL = "/settings"
 export const DASHBOARD_URL = "/dashboard";
 export const ADD_LIQUIDITY_URL = "/supply";
 export const REMOVE_LIQUIDITY_URL = "/remove-supply";
