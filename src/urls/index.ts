ff859f06ecc0987db552f8ea386c9b268d389c08
Žiga Franko <franko.ziga@gmail.com>
2021-09-28 15:57:12 +0200

    default urls added

---
+++ src/urls/index.ts
@@ -0,0 +1,5 @@
+export const SWAP_URL = "/swap";
+export const DASHBOARD_URL = "/dashboard";
+export const ADD_LIQUIDITY_URL = "/supply";
+export const REMOVE_LIQUIDITY_URL = "/remove-supply";
+export const CREATE_ERC20_TOKEN_URL = "/create-token";
