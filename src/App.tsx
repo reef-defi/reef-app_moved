5549fc94a3536873c710c282a97f814dfb56f12e
Žiga Franko <franko.ziga@gmail.com>
2021-10-05 12:18:47 +0200

    app update

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import Sidebar from './common/Sidebar';
 import Nav from './common/Nav';
 import ContentRouter from './pages/ContentRouter';
-import {hooks} from "@reef-defi/react-lib";
+import {availableNetworks, hooks} from "@reef-defi/react-lib";
+import { useAppSelector } from './store';
 
 const {useProvider} = hooks;
 
 const App = (): JSX.Element => {
-  const [provider, isProviderLoading, providerError] = useProvider("");
+  const [provider, isProviderLoading, providerError] = useProvider(availableNetworks.mainnet.rpcUrl);
   
   return (
     <div className="App d-flex w-100 h-100">
--- src/pages/ContentRouter.tsx
+++ src/pages/ContentRouter.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { ADD_LIQUIDITY_URL, CREATE_ERC20_TOKEN_URL, DASHBOARD_URL, REMOVE_LIQUIDITY_URL, SETTINGS_URL, SWAP_URL } from '../urls';
+import Swap from './swap/Swap';
 
 const ContentRouter = (): JSX.Element => (
   <div className="content">
     <Switch>
-      <Route exact path={SWAP_URL} render={() => (<div/>)} />
+      <Route exact path={SWAP_URL} component={Swap} />
       <Route exact path={SETTINGS_URL} render={() => (<div/>)} />
       <Route exact path={DASHBOARD_URL} render={() => (<div/>)} />
       <Route exact path={ADD_LIQUIDITY_URL} render={() => (<div/>)} />
