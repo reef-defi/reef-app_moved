8ae52d9c99f56295f1447de669bbc6a9151b7e97
Žiga Franko <franko.ziga@gmail.com>
2021-10-07 12:57:39 +0200

    pools is loading

--- src/store/actions/pools.ts
+++ src/store/actions/pools.ts
@@ -1,5 +1,5 @@
 import { Pool } from "@reef-defi/react-lib";
-import { RELOAD_POOLS, SET_POOLS_ACTION } from "../actionCreator";
+import { RELOAD_POOLS, SET_POOLS_ACTION, SET_POOLS_LOADING } from "../actionCreator";
 
 interface SetPools {
   type: typeof SET_POOLS_ACTION;
@@ -10,9 +10,15 @@ interface ReloadPools {
   type: typeof RELOAD_POOLS;
 };
 
+interface SetPoolsLoading {
+  type: typeof SET_POOLS_LOADING;
+  loading: boolean;
+}
+
 export type PoolsActions =
   | SetPools
-  | ReloadPools;
+  | ReloadPools
+  | SetPoolsLoading;
 
 export const setPools = (pools: Pool[]): SetPools => ({
   pools,
@@ -22,3 +28,8 @@ export const setPools = (pools: Pool[]): SetPools => ({
 export const reloadPools = (): ReloadPools => ({
   type: RELOAD_POOLS
 });
+
+export const setPoolsLoading = (loading: boolean): SetPoolsLoading => ({
+  loading,
+  type: SET_POOLS_LOADING
+})
