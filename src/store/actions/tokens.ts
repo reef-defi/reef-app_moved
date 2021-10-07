e1fabc0caa901b8cfa4f226b14f2ff2ac6baefa6
Žiga Franko <franko.ziga@gmail.com>
2021-10-07 12:57:46 +0200

    tokens is loading

--- src/store/actions/tokens.ts
+++ src/store/actions/tokens.ts
@@ -1,5 +1,5 @@
 import { Token } from "@reef-defi/react-lib";
-import { RELOAD_TOKENS, SET_TOKENS_ACTION } from "../actionCreator";
+import { RELOAD_TOKENS, SET_TOKENS_ACTION, SET_TOKENS_LOADING } from "../actionCreator";
 
 interface ReloadTokens {
   type: typeof RELOAD_TOKENS;
@@ -10,9 +10,15 @@ interface SetTokens {
   tokens: Token[];
 }
 
+interface SetTokensLoading {
+  type: typeof SET_TOKENS_LOADING;
+  loading: boolean;
+};
+
 export type TokensActions =
   | ReloadTokens
-  | SetTokens;
+  | SetTokens
+  | SetTokensLoading;
 
 export const reloadTokens = (): ReloadTokens => ({
   type: RELOAD_TOKENS,
@@ -21,4 +27,9 @@ export const reloadTokens = (): ReloadTokens => ({
 export const setTokens = (tokens: Token[]): SetTokens => ({
   tokens,
   type: SET_TOKENS_ACTION,
+});
+
+export const setTokensLoading = (loading: boolean): SetTokensLoading => ({
+  loading,
+  type: SET_TOKENS_LOADING
 });
--- src/store/reducers/tokens.ts
+++ src/store/reducers/tokens.ts
@@ -1,14 +1,16 @@
 import { Token } from "@reef-defi/react-lib";
-import { RELOAD_TOKENS, SET_TOKENS_ACTION } from "../actionCreator";
+import { RELOAD_TOKENS, SET_TOKENS_ACTION, SET_TOKENS_LOADING } from "../actionCreator";
 import { TokensActions } from "../actions/tokens";
 
 interface TokensReducer {
   tokens: Token[];
+  isLoading: boolean;
   reloadToggle: boolean;
 }
 
 const defaultState: TokensReducer = {
   tokens: [],
+  isLoading: false,
   reloadToggle: false,
 };
 
@@ -16,6 +18,7 @@ export default (state=defaultState, action: TokensActions): TokensReducer => {
   switch (action.type) {
     case RELOAD_TOKENS: return {...state, reloadToggle: !state.reloadToggle};
     case SET_TOKENS_ACTION: return {...state, tokens: [...action.tokens]};
+    case SET_TOKENS_LOADING: return {...state, isLoading: action.loading};
     default: return state;
   }
 }
