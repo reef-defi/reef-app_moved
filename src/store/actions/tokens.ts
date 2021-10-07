a3dc35b8cafac1ea93676d62719510f518a83105
Žiga Franko <franko.ziga@gmail.com>
2021-10-07 13:03:26 +0200

    Stores (#8)
    * reload actions creators and reload signers toggle
    
    * signers reload toggle reducer
    
    * tokens actions and reducer
    
    * pools actions and reducer
    
    * signers update
    
    * Add missing nginx config
    
    * removed reloadToggle from signers and added isLoading attribute to all reducers
    
    * pools is loading
    
    * tokens is loading
    
    * signers is loading
    
    * lint
    
    Co-authored-by: Furion <_@furion.me>

---
+++ src/store/actions/tokens.ts
@@ -0,0 +1,35 @@
+import { Token } from '@reef-defi/react-lib';
+import { RELOAD_TOKENS, SET_TOKENS_ACTION, SET_TOKENS_LOADING } from '../actionCreator';
+
+interface ReloadTokens {
+  type: typeof RELOAD_TOKENS;
+}
+
+interface SetTokens {
+  type: typeof SET_TOKENS_ACTION;
+  tokens: Token[];
+}
+
+interface SetTokensLoading {
+  type: typeof SET_TOKENS_LOADING;
+  loading: boolean;
+}
+
+export type TokensActions =
+  | ReloadTokens
+  | SetTokens
+  | SetTokensLoading;
+
+export const reloadTokens = (): ReloadTokens => ({
+  type: RELOAD_TOKENS,
+});
+
+export const setTokens = (tokens: Token[]): SetTokens => ({
+  tokens,
+  type: SET_TOKENS_ACTION,
+});
+
+export const setTokensLoading = (loading: boolean): SetTokensLoading => ({
+  loading,
+  type: SET_TOKENS_LOADING,
+});
