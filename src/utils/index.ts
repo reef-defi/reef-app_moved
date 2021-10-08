8a8b918fff856c4aac2f0116da7baeeb7bb74fa2
Žiga Franko <franko.ziga@gmail.com>
2021-10-08 13:02:56 +0200

    getSigner fun is now useGetSigner hook

--- src/utils/index.ts
+++
@@ -1,9 +0,0 @@
-import { useAppSelector } from "../store"
-import { Signer } from "@reef-defi/evm-provider";
-
-export const getSigner = (): Signer|undefined => {
-  const {selectedAccount, accounts} = useAppSelector((state) => state.signers);
-  return selectedAccount !== -1 
-    ? accounts[selectedAccount].signer
-    : undefined;
-}
