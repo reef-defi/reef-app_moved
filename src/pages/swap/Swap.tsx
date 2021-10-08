6a3eaf7ba1c4dd89e584396ef8721e62f46687db
Žiga Franko <franko.ziga@gmail.com>
2021-10-08 13:00:33 +0200

    Swap fixed in lib

--- src/pages/swap/Swap.tsx
+++ src/pages/swap/Swap.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 
 import { availableNetworks, Components } from '@reef-defi/react-lib';
+import { reloadTokens } from '../../store/actions/tokens';
+import { useAppDispatch, useAppSelector } from '../../store';
 
-const { SwapComponent, Display } = Components;
+const { SwapComponent } = Components;
 
-const none = (): void => {
-  const a = 1;
-};
+const none = (): void => {};
 
-const Swap = (): JSX.Element => (
-  <Display.CenterColumn>
-    <div style={{ minWidth: '500px' }}>
-      <SwapComponent
-        tokens={[]}
-        reloadTokens={none}
-        network={{ ...availableNetworks.mainnet }}
-        notify={none}
-      />
-    </div>
-  </Display.CenterColumn>
-);
+const Swap = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const reloadToggle = () => dispatch(reloadTokens())  
+  const {tokens} = useAppSelector((state) => state.tokens);
+
+  return (
+    <SwapComponent
+      tokens={tokens}
+      reloadTokens={reloadToggle}
+      network={{ ...availableNetworks.mainnet }}
+      notify={none}
+    />
+  );
+}
 
 export default Swap;
