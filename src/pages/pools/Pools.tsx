f2fe122251896a1de590fd5be4d84b9be991dfbc
Žiga Franko <franko.ziga@gmail.com>
2021-10-11 15:51:48 +0200

    Basic pools page

--- src/pages/pools/Pools.tsx
+++ src/pages/pools/Pools.tsx
@@ -1,7 +1,159 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Pools = (): JSX.Element => (
-  <div />
+import {Components, Pool, utils} from "@reef-defi/react-lib"
+import { useAppSelector } from '../../store';
+
+interface State {
+  pool: Pool;
+  toggle: () => void;
+}
+
+const CloseState = ({pool, toggle}: State): JSX.Element => (
+  <>
+    <Components.Display.FlexRow>
+      <Components.Icons.TokenIcon src={pool.token1.iconUrl} />
+      <Components.Icons.TokenIcon src={pool.token2.iconUrl} />
+      <Components.Display.CenterRow>
+        <Components.Display.MS size="2">
+          <Components.Text.LeadText>
+            {pool.token1.name}/{pool.token2.name}
+          </Components.Text.LeadText>
+        </Components.Display.MS>
+      </Components.Display.CenterRow>
+    </Components.Display.FlexRow>
+    <Components.Display.FlexColumn>
+      <Components.Text.MiniText>
+        <Components.Text.MutedText>Liquidity:</Components.Text.MutedText>
+      </Components.Text.MiniText>
+      <Components.Text.Text>{utils.convert2Normal(pool.decimals, pool.minimumLiquidity).toFixed(4)}</Components.Text.Text>
+    </Components.Display.FlexColumn>
+    <Components.Display.FlexColumn>
+      <Components.Text.MiniText>
+        <Components.Text.MutedText>Supply:</Components.Text.MutedText>
+      </Components.Text.MiniText>
+      <Components.Text.Text>{utils.convert2Normal(pool.decimals, pool.totalSupply).toFixed(4)}</Components.Text.Text>
+    </Components.Display.FlexColumn>
+    <Components.Button.EmptyButton onClick={toggle}>
+      <Components.Icons.DownIcon />
+    </Components.Button.EmptyButton>
+  </>
 );
+
+const OpenState = ({pool, toggle}: State): JSX.Element => {
+
+  return (
+    <Components.Display.FullColumn>
+      <Components.Display.ContentBetween>
+        <Components.Display.FlexRow>
+          <Components.Icons.TokenIcon src={pool.token1.iconUrl} />
+          <Components.Icons.TokenIcon src={pool.token2.iconUrl} />
+          <Components.Display.CenterRow>
+            <Components.Display.MS size="2">
+              <Components.Text.LeadText>
+                {pool.token1.name}/{pool.token2.name}
+              </Components.Text.LeadText>
+            </Components.Display.MS>
+          </Components.Display.CenterRow>
+        </Components.Display.FlexRow>
+        <Components.Button.EmptyButton onClick={toggle}>
+          <Components.Icons.UpIcon />
+        </Components.Button.EmptyButton>
+      </Components.Display.ContentBetween>
+      <Components.Display.ContentBetween>
+        <div className="w-50 me-1 p-3 border border-1 border-rad">Basic Chart WIP...</div>
+        <div className="w-50 ms-1">
+          <Components.Card.SubCard>
+            <Components.Label.ConfirmLabel 
+              title="Supply: "
+              value={utils.convert2Normal(pool.decimals, pool.totalSupply).toFixed(4)}
+            />
+            <Components.Label.ConfirmLabel 
+              title="Liquidity: "
+              value={utils.convert2Normal(pool.decimals, pool.minimumLiquidity).toFixed(4)}
+            />
+            <Components.Label.ConfirmLabel 
+              title="Locked 1: "
+              value={utils.toBalance(pool.token1).toFixed(4)}
+            />
+            <Components.Label.ConfirmLabel 
+              title="Locked 2: "
+              value={utils.toBalance(pool.token2).toFixed(4)}
+            />
+            <Components.Label.ConfirmLabel 
+              title="Your share: "
+              value={`${utils.calculatePoolShare(pool).toFixed(4)} %`}
+            />
+          </Components.Card.SubCard>
+        </div>
+      </Components.Display.ContentBetween>
+      <Components.Display.MT size="2" />
+      <Components.Display.ContentEnd>
+        <Components.Button.Button>Open pool</Components.Button.Button>
+      </Components.Display.ContentEnd>
+    </Components.Display.FullColumn>
+  )
+}
+
+const Pools = (): JSX.Element => {
+  const {isLoading, pools} = useAppSelector((state) => state.pools);
+  const [isOpen, setIsOpen] = useState<boolean[]>([]);
+
+  const closeAll = () => {
+    setIsOpen(pools.map(() => false))
+  };
+  const open = (index: number) => {
+    closeAll();
+    setIsOpen([
+      ...isOpen.slice(0, index),
+      true,
+      ...isOpen.slice(index+1, isOpen.length)
+    ]);
+  };
+  useEffect(() => {
+    closeAll();
+  }, [pools]);
+  
+
+  const poolsView = pools
+    .map((pool, index) => (
+      <Components.List.ListItem key={pool.poolAddress}>
+        {isOpen[index] 
+          ? <OpenState
+            pool={pool}
+            toggle={closeAll}
+          />
+          : <CloseState 
+            pool={pool}
+            toggle={() => open(index)}
+          />
+        }
+      </Components.List.ListItem>
+    ));
+
+  return (
+    <Components.Display.CenterColumn>
+      <Components.Display.MinWidth size={500}>
+        <Components.Card.Card>
+          <Components.Card.CardHeader>
+            <Components.Card.CardTitle title="Pools" />
+            <Components.Display.FlexRow>
+              <Components.Button.Button>Remove supply</Components.Button.Button>
+              <Components.Display.MS size="1" />
+              <Components.Button.Button>Add supply</Components.Button.Button>
+            </Components.Display.FlexRow>
+          </Components.Card.CardHeader>
+          <Components.Display.MT size="3">
+            {/* .default is kind of lame to write.... */}
+            <Components.List.default> 
+              <Components.List.ListEmptyItem />
+              {isLoading ? <Components.Loading.Loading /> : poolsView}
+              <Components.List.ListEmptyItem />
+            </Components.List.default>
+          </Components.Display.MT>
+        </Components.Card.Card>
+      </Components.Display.MinWidth>
+    </Components.Display.CenterColumn>
+  )
+};
 
 export default Pools;
