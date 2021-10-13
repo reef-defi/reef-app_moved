cb8b14648a9ed32a9ee6f36e725f1be4adace71e
Žiga Franko <franko.ziga@gmail.com>
2021-10-13 11:17:41 +0200

    pass signer in swap component

--- src/pages/swap/Swap.tsx
+++ src/pages/swap/Swap.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { availableNetworks, Components } from '@reef-defi/react-lib';
 import { reloadTokens } from '../../store/actions/tokens';
 import { useAppDispatch, useAppSelector } from '../../store';
+import { useGetSigner } from '../../hooks/useGetSigner';
 
 const { SwapComponent } = Components;
 
@@ -13,6 +14,8 @@ const Swap = (): JSX.Element => {
   const dispatch = useAppDispatch();
   const { tokens } = useAppSelector((state) => state.tokens);
 
+  const selectedAccount = useGetSigner();
+
   const reloadToggle = (): void => {
     dispatch(reloadTokens());
   };
@@ -20,6 +23,7 @@ const Swap = (): JSX.Element => {
   return (
     <SwapComponent
       tokens={tokens}
+      account={selectedAccount}
       reloadTokens={reloadToggle}
       network={{ ...availableNetworks.mainnet }}
       notify={none}
