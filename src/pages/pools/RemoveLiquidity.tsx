ca985e104f124bfd9d37cc9d3f29f00de13dfd19
Žiga Franko <franko.ziga@gmail.com>
2021-10-19 11:08:53 +0200

    remove liquidity component

---
+++ src/pages/pools/RemoveLiquidity.tsx
@@ -0,0 +1,47 @@
+import React from "react"
+import {availableNetworks, Components, Token} from "@reef-defi/react-lib";
+import { Redirect, useHistory, useParams } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../store";
+import { useGetSigner } from "../../hooks/useGetSigner";
+import { POOLS_URL } from "../../urls";
+import { reloadTokens } from "../../store/actions/tokens";
+
+const {RemoveLiquidityComponent} = Components;
+
+interface UrlParams {
+  address1: string;
+  address2: string;
+}
+
+const findToken = (address: string, tokens: Token[]): Token|undefined => 
+  tokens.find((token) => token.address === address);
+
+
+const RemoveLiquidity = (): JSX.Element => {
+  const history = useHistory();
+  const dispatch = useAppDispatch();
+  const { address1, address2 } = useParams<UrlParams>();
+  const {tokens} = useAppSelector((state) => state.tokens);
+  
+  const signer = useGetSigner();
+  const token1 = findToken(address1, tokens);
+  const token2 = findToken(address2, tokens);
+
+  const back = () => history.push(POOLS_URL);
+  const reload = () => dispatch(reloadTokens());
+
+  return (!token1 || !token2
+    ? <Redirect to={POOLS_URL} />
+    : <RemoveLiquidityComponent
+      token1={token1}
+      token2={token2}
+      signer={signer}
+      network={availableNetworks.mainnet}
+      back={back}
+      notify={(message, type) => {}}
+      reloadTokens={reload}
+    />
+  );
+}
+
+export default RemoveLiquidity;
