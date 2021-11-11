21913524dc64532777a718df76c17ed1a8dffb27
Matjaz Hirsman <matjazonline@gmail.com>
2021-11-11 10:22:22 +0100

    Dashboard tokens (#12)

---
+++ src/hooks/useSignerTokenBalances.ts
@@ -0,0 +1,88 @@
+import { Pool, reefTokenWithAmount, Token } from '@reef-defi/react-lib';
+import { useEffect, useState } from 'react';
+import { BigNumber, utils } from 'ethers';
+
+const { parseUnits, formatEther } = utils;
+
+export interface TokenWithPrice extends Token {
+  price: ValueWithStatus;
+  balanceValue: ValueWithStatus;
+}
+
+export enum ValueStatus {
+  LOADING= 'LOADING',
+  NO_DATA = 'NO_DATA'
+}
+
+export type ValueWithStatus = number | ValueStatus;
+
+// eslint-disable-next-line no-prototype-builtins
+export const isValueWithStatusSet = (priceValue: ValueWithStatus): boolean => !ValueStatus.hasOwnProperty(priceValue);
+
+const calculateBalanceValue = (price: ValueWithStatus, token: Token): ValueWithStatus => {
+  // eslint-disable-next-line no-prototype-builtins
+  if (!isValueWithStatusSet(price)) {
+    return price;
+  }
+  const priceBN = BigNumber.from(parseUnits(price.toString()));
+  const balanceFixed = parseInt(formatEther(token.balance.toString()), 10);
+  return parseFloat(formatEther(priceBN.mul(BigNumber.from(balanceFixed)).toString()));
+};
+
+const getReefTokenPoolReserves = (reefTokenPool: Pool, reefAddress: string): {reefReserve:number, tokenReserve: number} => {
+  let reefReserve: number;
+  let tokenReserve: number;
+  if (reefTokenPool.token1.address.toLowerCase() === reefAddress.toLowerCase()) {
+    reefReserve = parseInt(reefTokenPool.reserve1, 10);
+    tokenReserve = parseInt(reefTokenPool.reserve2, 10);
+  } else {
+    reefReserve = parseInt(reefTokenPool.reserve2, 10);
+    tokenReserve = parseInt(reefTokenPool.reserve1, 10);
+  }
+  return { reefReserve, tokenReserve };
+};
+
+const findReefTokenPool = (pools: Pool[], reefAddress: string, token: Token): Pool | undefined => pools.find((pool) => (pool.token1.address.toLowerCase() === reefAddress.toLowerCase() && pool.token2.address.toLowerCase() === token.address.toLowerCase()) || (pool.token2.address.toLowerCase() === reefAddress.toLowerCase() && pool.token1.address.toLowerCase() === token.address.toLowerCase()));
+
+const calculateTokenPrice = (token: Token, pools: Pool[], reefPrice: ValueWithStatus): ValueWithStatus => {
+  if (!isValueWithStatusSet(reefPrice)) {
+    return reefPrice;
+  }
+  const { address: reefAddress } = reefTokenWithAmount();
+  let ratio: number;
+  if (token.address.toLowerCase() !== reefAddress.toLowerCase()) {
+    const reefTokenPool = findReefTokenPool(pools, reefAddress, token);
+    if (reefTokenPool) {
+      const { reefReserve, tokenReserve } = getReefTokenPoolReserves(reefTokenPool, reefAddress);
+      ratio = reefReserve / tokenReserve;
+      return ratio * (reefPrice as number);
+    }
+    return ValueStatus.NO_DATA;
+  }
+  return reefPrice || ValueStatus.NO_DATA;
+};
+
+export const useSignerTokenBalances = (tokens: Token[], pools: Pool[], reefPrice: ValueWithStatus): TokenWithPrice[] => {
+  const [balances, setBalances] = useState<TokenWithPrice[]>([]);
+  useEffect(() => {
+    if (!tokens.length) {
+      setBalances([]);
+      return;
+    }
+    if (!pools.length || !isValueWithStatusSet(reefPrice)) {
+      setBalances(tokens.map((tkn) => {
+        const stat = !isValueWithStatusSet(reefPrice) ? reefPrice : ValueStatus.LOADING;
+        return { ...tkn, balanceValue: stat, price: stat } as TokenWithPrice;
+      }));
+      return;
+    }
+
+    const bal = tokens.map((token) => {
+      const price = calculateTokenPrice(token, pools, reefPrice);
+      const balanceValue = calculateBalanceValue(price, token);
+      return { ...token, price, balanceValue };
+    });
+    setBalances(bal);
+  }, [tokens, pools, reefPrice]);
+  return balances;
+};
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { api, Components } from '@reef-defi/react-lib';
+import { useAppSelector } from '../../store';
+import { useLoadSignerTokens } from '../../hooks/useLoadSignerTokens';
+import { TokenBalances } from './TokenBalances';
+import {
+  isValueWithStatusSet,
+  TokenWithPrice,
+  useSignerTokenBalances,
+  ValueStatus, ValueWithStatus,
+} from '../../hooks/useSignerTokenBalances';
+import { toCurrencyFormat } from '../../utils/utils';
+import { Balance } from './Balance';
+import { SendIcon } from '../../common/Icons';
+import { ActionButtons } from './ActionButtons';
+import { useGetSigner } from '../../hooks/useGetSigner';
 
-const Dashboard = (): JSX.Element => (
-  <div />
-);
+const { retrieveReefCoingeckoPrice } = api;
+const { Loading } = Components.Loading;
+
+const Dashboard = (): JSX.Element => {
+  const history = useHistory();
+  const { isLoading: tokensLoading } = useAppSelector((state) => state.tokens);
+  const { pools } = useAppSelector((state) => state.pools);
+  const selectedSigner = useGetSigner();
+  const signerTokens = useLoadSignerTokens(selectedSigner);
+  const [reefPrice, setReefPrice] = useState<number|ValueStatus>(ValueStatus.LOADING);
+  const signerTokenBalances = useSignerTokenBalances(signerTokens, pools, reefPrice);
+
+  const totalBalance = signerTokenBalances.reduce((state: ValueWithStatus, curr) => {
+    if (Number.isNaN(curr.balanceValue) || !isValueWithStatusSet(curr.balanceValue)) {
+      return state;
+    }
+    if (!Number.isNaN(+curr.balanceValue as number) && isValueWithStatusSet(curr.balanceValue)) {
+      const stateNr = isValueWithStatusSet(state) ? state as number : 0;
+      return stateNr + (curr.balanceValue as number);
+    }
+    return state;
+  }, ValueStatus.LOADING);
+
+  useEffect(() => {
+    const getPrice = async ():Promise<void> => {
+      let price: number|ValueStatus = ValueStatus.NO_DATA;
+      try {
+        price = await retrieveReefCoingeckoPrice();
+      } catch (e) {
+      }
+      setReefPrice(price);
+    };
+    const interval = setInterval(getPrice, 30000);
+    getPrice();
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div className="w-100">
+      <div className="mb-4 row">
+        <Balance balance={totalBalance} />
+        <ActionButtons />
+      </div>
+      <TokenBalances tokens={signerTokenBalances} />
+    </div>
+  );
+};
 
 export default Dashboard;
