be9e7d70e4b85f7049961d086d908f191e67a850
Matjaz Hirsman <matjazonline@gmail.com>
2022-01-31 21:56:16 +0100

    wait for contract to be indexed before verification (#33)

--- src/utils/contract.ts
+++ src/utils/contract.ts
@@ -1,6 +1,11 @@
 import axios, { AxiosResponse } from 'axios';
 import { Contract } from 'ethers';
-import { delay } from './utils';
+import { gql } from '@apollo/client';
+import {
+  Observable, switchMap, take, timeout, catchError, of, map, tap, firstValueFrom, skipWhile,
+} from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { apolloClientInstance$ } from './apolloConfig';
 
 const CONTRACT_VERIFICATION_URL = 'api/verificator/submit-verification';
 
@@ -26,12 +31,37 @@ export interface ReefContract extends BaseContract {
 
 const contractVerificatorApi = axios.create();
 
+const CONTRACT_EXISTS_GQL = gql`
+  subscription query ($address: String!) {
+            contract(
+              where: { address: { _eq: $address } }
+            ) {
+              address
+            }
+          }
+`;
+const isContractIndexed$ = (address: string): Observable<boolean> => apolloClientInstance$.pipe(
+  timeout(120000),
+  switchMap((apollo) => apollo.subscribe({
+    query: CONTRACT_EXISTS_GQL,
+    variables: { address },
+    fetchPolicy: 'network-only',
+  })),
+  map((res:any) => res.data.contract && res.data.contract.length),
+  skipWhile((v) => !v),
+  catchError(() => of(false)),
+  take(1),
+);
+
 export const verifyContract = async (deployedContract: Contract, contract: ReefContract, arg: string[], url?: string): Promise<boolean> => {
   if (!url) {
     return false;
   }
+
   try {
-    await delay(5000);
+    if (!await firstValueFrom(isContractIndexed$(deployedContract.address.toLowerCase()))) {
+      return false;
+    }
     const body: VerificationContractReq = {
       address: deployedContract.address,
       arguments: JSON.stringify(arg),
