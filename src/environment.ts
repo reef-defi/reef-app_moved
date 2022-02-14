b2427fa33c55a37b913725d7394a0327cec5c5b7
Matjaz Hirsman <matjazonline@gmail.com>
2022-02-14 09:24:44 -0700

    local env setting

--- src/environment.ts
+++ src/environment.ts
@@ -1,12 +1,12 @@
 import { availableNetworks, Network, appState } from '@reef-defi/react-lib';
 
-export const currentNetwork = availableNetworks.mainnet; /* || availableNetworks.testnet ||  {
+export const currentNetwork = /* availableNetworks.testnet || */ /* availableNetworks.mainnet;  ||  */ {
   name: 'testnet',
   rpcUrl: 'ws://localhost:9944',
   reefscanUrl: 'http://localhost:8000',
   factoryAddress: '0xcA36bA38f2776184242d3652b17bA4A77842707e',
   routerAddress: '0x0A2906130B1EcBffbE1Edb63D5417002956dFd41',
-} as Network; */
+} as Network;
 
 export const getGQLUrls = (network: Network): {ws:string, http:string} => {
   const { testnet } = availableNetworks;
