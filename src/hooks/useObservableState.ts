6d5423a83537959e8caff848f921d04f2982db80
Matjaz Hirsman <matjazonline@gmail.com>
2022-02-15 10:44:40 -0700

    fix useObservableState hook state

--- src/hooks/useObservableState.ts
+++ src/hooks/useObservableState.ts
@@ -1,26 +1,16 @@
 import { Observable, Subscription } from 'rxjs';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export const useObservableState = <T>(observable: Observable<T>): T | undefined => {
   const [value, setValue] = useState<T>();
-  let subs: Subscription;
+  const subs = useRef<Subscription>();
+
   useEffect(() => {
-    subs?.unsubscribe();
-    subs = observable.subscribe((s) => {
+    subs.current?.unsubscribe();
+    subs.current = observable.subscribe((s) => {
       setValue(s);
     });
-    return () => subs?.unsubscribe();
+    return () => subs.current?.unsubscribe();
   }, [observable]);
   return value;
 };
-
-/*
-export const useObservableStateProgress = <T>(observable: Observable<DataWithProgress<T>>): utils.DataWithProgress<T> => {
-  const [value, setValue] = useState<utils.DataWithProgress<T>>(utils.DataProgress.LOADING);
-  useEffect(() => {
-    const sub = observable.subscribe(s =>setValue(s));
-    return () => sub.unsubscribe();
-  });
-  return value;
-};
-*/
