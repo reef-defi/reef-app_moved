683d8025be8d6c838ae0b9f3a07ee761ef1d8e9e
Samo Šlag <samo.slag@gmail.com>
2022-02-24 17:32:24 +0100

    Fix alignment in TokenPill

--- src/pages/dashboard/TokenPill.tsx
+++ src/pages/dashboard/TokenPill.tsx
@@ -22,27 +22,30 @@ export const TokenPill = ({ token }: TokenPill): JSX.Element => {
       <div className="token-balance-item radius-border d-flex d-flex-space-between d-flex-vert-center">
         <div className="token-balance-item_icon-text mr-1">
           <div className="token-balance-item_icon-text_w mr-1"><img src={token.iconUrl ? token.iconUrl : getIconUrl(token.address)} alt={token.name} /></div>
-          <div className="">
-            <div className="title-font text-bold ">{token.name}</div>
-            <div className="token-balance-item_sub-title">
-              <span>{showBalance(token)}</span>
+          <div className='token-balance-item__info'>
+            <div className="">
+              <div className="title-font text-bold ">{token.name}</div>
+              <div className="token-balance-item_sub-title">
+                <span>{showBalance(token)}</span>
 
-              <span className="token-balance-item__price">
-                {isDataSet(token.price) && toCurrencyFormat(token.price as number, { maximumFractionDigits: token.price < 1 ? 4 : 2 }) }
-                {!isDataSet(token.price) && <span>No pool data</span> }
-                {/* TODO {!isDataSet(token.price) && token.price === DataProgress.LOADING && <Loading />} */}
-                {/* TODO {!isDataSet(token.price) && token.price === DataProgress.NO_DATA && ' - '} */}
-              </span>
+                <span className="token-balance-item__price">
+                  {isDataSet(token.price) && toCurrencyFormat(token.price as number, { maximumFractionDigits: token.price < 1 ? 4 : 2 }) }
+                  {!isDataSet(token.price) && <span>No pool data</span> }
+                  {/* TODO {!isDataSet(token.price) && token.price === DataProgress.LOADING && <Loading />} */}
+                  {/* TODO {!isDataSet(token.price) && token.price === DataProgress.NO_DATA && ' - '} */}
+                </span>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="token-balance-item_balances title-font text-bold text-color-dark-accent">
-          <div>
-            {isDataSet(balanceValue) && (
-            <div className="token-balance-item__balance">
-              {toCurrencyFormat(balanceValue as number)}
+
+            <div className="token-balance-item_balances title-font text-bold text-color-dark-accent">
+              <div>
+                {isDataSet(balanceValue) && (
+                <div className="token-balance-item__balance">
+                  {toCurrencyFormat(balanceValue as number)}
+                </div>
+                ) }
+              </div>
             </div>
-            ) }
           </div>
         </div>
       </div>
