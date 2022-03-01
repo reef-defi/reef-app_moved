963240ca8a5f33a4bef2aed4b49e4b33b664d332
Samo Šlag <samo.slag@gmail.com>
2022-03-01 17:47:31 +0100

    Add Token skeleton loader

--- src/pages/dashboard/TokenBalances.tsx
+++ src/pages/dashboard/TokenBalances.tsx
@@ -4,12 +4,41 @@ import { TokenPill } from './TokenPill';
 
 const { isDataSet, DataProgress } = utils;
 
-const { Loading } = Components.Loading;
-
 interface TokenBalances {
     tokens: utils.DataWithProgress<TokenWithAmount[]>;
     onRefresh?: any;
 }
+
+export const Skeleton = (): JSX.Element => (
+  <div className="col-md-12 col-lg-6 token-skeleton">
+    <div className="token-balance-item radius-border d-flex d-flex-space-between d-flex-vert-center">
+      <div className="token-balance-item_icon-text mr-1">
+        <div className="token-balance-item_icon-text_w mr-1">
+          <div className='token-skeleton__image'/>
+        </div>
+        <div className='token-balance-item__info'>
+          <div className="">
+            <div className="title-font text-bold ">
+              <div className='token-skeleton__title'/>
+            </div>
+            <div className="token-balance-item_sub-title">
+              <span>
+                <div className='token-skeleton__sub-title'/>
+              </span>
+            </div>
+          </div>
+
+          <div className="token-balance-item_balances title-font text-bold text-color-dark-accent">
+            <div>
+              <div className='token-skeleton__balance'/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
 export const TokenBalances = ({ tokens, onRefresh }: TokenBalances): JSX.Element => (
   <div className="row">
     <div className="mb-4 col-12 d-flex d-flex-space-between d-flex-vert-base">
@@ -35,12 +64,12 @@ export const TokenBalances = ({ tokens, onRefresh }: TokenBalances): JSX.Element
 
     </div>
     <div className="col-12">
-      {(!isDataSet(tokens) && tokens === DataProgress.LOADING)
-          && (
-          <div className="mt-5">
-            <Loading />
-          </div>
-          )}
+      {!tokens && (
+            <div className="row tokens-container">
+              <Skeleton />
+              <Skeleton />
+            </div>
+        )}
       { tokens && !!isDataSet(tokens) && (
       <div className="row tokens-container" style={{ maxHeight: 'auto' }}>
         {(tokens as TokenWithAmount[]).map((token: TokenWithAmount) => (<TokenPill token={token} key={token.address} />
