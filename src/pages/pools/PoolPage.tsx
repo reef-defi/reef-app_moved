273ea7b5806f11d74bda1bea16810ae40c958518
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-03-04 18:04:35 +0100

    pool info querying and showing (tokens, liquidity, supply, volume)

--- src/pages/pools/PoolPage.tsx
+++ src/pages/pools/PoolPage.tsx
@@ -3,7 +3,8 @@ import { useParams } from "react-router-dom";
 import {useSubscription, useQuery, gql} from "@apollo/client"
 import {Components} from "@reef-defi/react-lib"
 import { getIconUrl } from "../../utils/utils";
-
+import { Skeleton } from "../dashboard/TokenBalances";
+import { BigNumber, utils } from "ethers";
 const {Text, ColorText, BoldText, LeadText} = Components.Text;
 
 interface PoolPage {
@@ -71,12 +72,56 @@ subscription transactions($address: String!, $type: String_comparison_exp!) {
     amount_2
     amount_in_1
     amount_in_2
+    total_supply
     timestamp
   }
 }
 `
 
+const POOL_RESERVES_SUBSCRIPTION_GQL = gql`
+query pool_event($address: String!) {
+  pool_event(
+    where: {
+      pool: { address: {_ilike: $address } }
+      type: { _eq: "Sync" }
+    }
+    order_by: { timestamp: desc }
+    limit: 1
+  ) {
+    reserved_1
+    reserved_2
+  }
+}
+`
+const POOL_SUPPLY_GQL = gql`
+query pool_supply($address: String!) {
+  pool_day_supply(
+    where: {
+      pool: { address: {_ilike: $address } }
+    }
+    order_by: { timeframe: desc }
+    limit: 1
+  ) {
+    total_supply
+    supply
+  }
+}
+`
 
+const POOL_VOLUME_GQL = gql`
+query pool_volume($address: String!) {
+  pool_day_volume(
+    where: {
+      pool: { address: {_ilike: $address } }
+    }
+    order_by: { timeframe: desc }
+    limit: 2
+  ) {
+    amount_1
+    amount_2
+  }
+}
+`
 
 
 interface CandlestickData {
@@ -96,19 +141,108 @@ interface CandlestickData {
   }
 }
 
+interface ContractData {
+  symbol: string;
+  name: string;
+  decimals: number
+}
+
+interface PoolData {
+  id: number;
+  address: string;
+  token_contract_1: {
+    address: string;
+    verified_contract: {
+      contract_data: ContractData;
+    };
+  };
+  token_contract_2: {
+    address: string;
+    verified_contract: {
+      contract_data: ContractData;
+    };
+  };
+}
+
+interface Reserves {
+  reserved_1: number;
+  reserved_2: number;
+  total_supply: number;
+}
+
+interface Supply {
+  total_supply: number;
+  supply: number;
+}
+interface Volume {
+  amount_1: number;
+  amount_2: number;
+}
+
+type VolumeQuery = { pool_day_volume: Volume[] };
+
+interface SupplyQuery {
+  pool_day_supply: Supply[]
+}
+interface PoolQuery {
+  pool: PoolData[];
+}
+interface ReservesQuery {
+  pool_event: Reserves[];
+}
+interface CandlestickQuery {
+  pool_minute_candlestick: CandlestickData[];
+}
+interface AddressVar {
+  address: string;
+}
+
 type Transaction = "Swap" | "Mint" | "Burn" | "All"
+
+const toHumanAmount = (amount: string): string => {
+  const head = amount.slice(0, amount.indexOf("."));
+  const amo = amount.replace(".", "");
+  
+  if (head.length > 9) {
+    return `${amo.slice(0, head.length-9)}.${amo.slice(head.length-9, head.length-9+2)} B`
+  }
+  if (head.length > 6) {
+    return `${amo.slice(0, head.length-6)}.${amo.slice(head.length-6, head.length-6+2)} M`
+  }
+  if (head.length > 3) {
+    return `${amo.slice(0, head.length-3)}.${amo.slice(head.length-3, head.length-3+2)} k`
+  }
+  return amount;
+}
+
+const formatAmount = (amount: number, decimals: number): string => toHumanAmount(
+  utils.formatUnits(
+    BigNumber.from(
+      amount.toLocaleString('fullwide', {useGrouping:false})
+    ).toString(),
+    decimals
+  )
+)
+const percentageDifference = (prev: number, next: number): number => {
+  // const a1 = BigNumber.from(prev.toLocaleString('fullwide', {useGrouping:false}))
+  // const a2 = BigNumber.from(next.toLocaleString('fullwide', {useGrouping:false}))
+  // console.log(a1)
+  // console.log(a2)
+  // const diff = a2.sub(a1).mul(1000).div(a1)
+  return (next - prev)/prev * 100//diff.toNumber() / 10;
+}
+
 
 const PoolPage = ({} : PoolPage): JSX.Element => {
   const [type, setType] = useState<Transaction>("All");
   const {address} = useParams<UrlParam>();
-  const token1Icon = getIconUrl(address);
-  const {loading: candlestickLoading, data: candlestickData, error: candlestickError} = useSubscription<CandlestickData[]>(
+  const {loading: candlestickLoading, data: candlestickData, error: candlestickError} = useSubscription<CandlestickQuery, AddressVar>(
     MINUTE_CANDLESTICK_GQL, 
     { variables: { address } }
   );
-  const {loading: loadingPool, data: poolData, error: poolError} = useQuery(
+  const {loading: loadingPool, data: poolData, error: poolError} = useQuery<PoolQuery, AddressVar>(
     POOL_GQL,
-    {variables: { address }}
+    {variables: { address } }
   )
   const {loading: loadingTransaction, data: transactionData, error: transactionError} = useSubscription(
     POOL_TRANSACTIONS_GQL,
@@ -116,52 +250,144 @@ const PoolPage = ({} : PoolPage): JSX.Element => {
       address,
       type: { _in: type === "All" ? ["Swap", "Mint", "Burn"] : [type] }
     }}
+  );
+  const {loading: loadingReserves, data: reservesData, error: reservesError} = useQuery<ReservesQuery, AddressVar>(
+    POOL_RESERVES_SUBSCRIPTION_GQL,
+    { variables: { address } }
+  )
+  const {loading: loadingSupply, data: supplyData, error: supplyError} = useQuery<SupplyQuery, AddressVar>(
+    POOL_SUPPLY_GQL,
+    { variables: { address } }
+  )
+  const {loading: loadingVolume, data: volumeData, error: volumeError} = useQuery<VolumeQuery, AddressVar>(
+    POOL_VOLUME_GQL,
+    { variables: { address } }
   )
 
-  // useEffect(async () => {
+  // Token info
+  const poolExists = poolData && poolData.pool.length > 0;
+  const tokenAddress1 = poolExists
+    ? poolData.pool[0].token_contract_1.address
+    : "0x";
+  const tokenAddress2 = poolExists
+    ? poolData.pool[0].token_contract_2.address
+    : "0x";
+  const tokenSymbol1 = poolExists
+    ? poolData.pool[0].token_contract_1.verified_contract.contract_data.symbol
+    : "-";
+  const tokenSymbol2 = poolExists
+    ? poolData.pool[0].token_contract_2.verified_contract.contract_data.symbol
+    : "-";
+  const tokenIcon1 = poolExists
+    ? getIconUrl(tokenAddress1)
+    : "";
+  const tokenIcon2 = poolExists
+    ? getIconUrl(tokenAddress2)
+    : "";
+  
+  const decimal1 = poolExists
+    ? poolData.pool[0].token_contract_1.verified_contract.contract_data.decimals
+    : 1;
+  const decimal2 = poolExists
+    ? poolData.pool[0].token_contract_2.verified_contract.contract_data.decimals
+    : 1;
 
+  // Reserves
+  const reserved1 = 
+    reservesData && reservesData.pool_event.length > 0
+      ? formatAmount(reservesData.pool_event[0].reserved_1, decimal1) 
+      : "-"
+  const reserved2 = 
+    reservesData && reservesData.pool_event.length > 0
+      ? formatAmount(reservesData.pool_event[0].reserved_2, decimal2) 
+      : "-"
+      
 
-  // }, [address])
+  // Supply
+  const totalSupply = 
+    supplyData && supplyData.pool_day_supply.length > 0
+      ? formatAmount(supplyData.pool_day_supply[0].total_supply, 18) 
+      : "-"
 
+  const totalSupplyPercentage = 
+    supplyData && supplyData.pool_day_supply.length > 0
+    ? supplyData.pool_day_supply[0].supply / supplyData.pool_day_supply[0].total_supply * 100 // this 1 is a hack
+    : 0;
+
+  // Volume
+  const volume1 =
+    volumeData && volumeData.pool_day_volume.length > 0 && decimal1 != 1
+      ? formatAmount(volumeData.pool_day_volume[0].amount_1, decimal1)
+      : "-";
+
+  const volume2 =
+    volumeData && volumeData.pool_day_volume.length > 0 && decimal2 != 1
+      ? formatAmount(volumeData.pool_day_volume[0].amount_2, decimal2)
+      : "-";
+  const volumeDifference1 =
+    volumeData && volumeData.pool_day_volume.length > 1
+    ? (
+        volumeData.pool_day_volume[1].amount_1 - 
+        volumeData.pool_day_volume[0].amount_1
+      ) / volumeData.pool_day_volume[0].amount_1 * 100
+    : 0;
+  const volumeDifference2 =
+    volumeData && volumeData.pool_day_volume.length > 1
+    ? (
+        volumeData.pool_day_volume[1].amount_2 - 
+        volumeData.pool_day_volume[0].amount_2
+      ) / volumeData.pool_day_volume[0].amount_2 * 100
+    : 0;
+  
   return (
     <div className="w-100 row justify-content-center">
       <div className="col-xl-8 col-lg-10 col-md-12">
-        <div className="d-flex ms-1 mb-1">
-          <Components.Icons.TokenIcon src=""/>
-          <Components.Icons.TokenIcon src=""/>
+        {
+          loadingPool || !poolData
+            ? <Skeleton />
+            : <div className="d-flex ms-1 mb-1">
+                <Components.Icons.TokenIcon src={tokenIcon1}/>
+                <Components.Icons.TokenIcon src={tokenIcon2}/>
 
-          <LeadText>
-            Token1 / Token2
-          </LeadText>
-        </div>
+                <LeadText>
+                  { tokenSymbol1 } / { tokenSymbol2 }
+                </LeadText>
+              </div>
+          }
 
         <Components.Display.FullRow>
           <Components.Display.ContentBetween>
             <div className="d-flex my-2">
               <div className="card border-rad">
                 <div className="card-body py-1">
-                  <div className="d-flex">
-                    <Components.Icons.TokenIcon src=""/>
-                    <Components.Display.ME size="1" />
-                    <LeadText>1 Token1 = n Token2</LeadText>
-                  </div>
+                  { loadingPool  || !poolData
+                    ? <Skeleton />
+                    : <div className="d-flex">
+                        <Components.Icons.TokenIcon src={tokenIcon1}/>
+                        <Components.Display.ME size="1" />
+                        <LeadText>1 {tokenSymbol1} = n {tokenSymbol2} </LeadText>
+                      </div>
+                  }
                 </div>
               </div>
               <Components.Display.ME size="1" />
 
               <div className="card border-rad">
                 <div className="card-body py-1">
-                  <div className="d-flex">
-                    <Components.Icons.TokenIcon src=""/>
-                    <Components.Display.ME size="1" />
-                    <LeadText>1 Token1 = n Token2</LeadText>
-                  </div>
+                  {
+                    loadingPool || !poolData
+                    ? <Skeleton />
+                    : <div className="d-flex">
+                        <Components.Icons.TokenIcon src={tokenIcon2}/>
+                        <Components.Display.ME size="1" />
+                        <LeadText>1 {tokenSymbol2} = n {tokenSymbol1}</LeadText>
+                      </div>
+                  }
                 </div>
               </div>
             </div>
 
             <div className="d-flex">
-              {/* <button className="btn">Add liquidity</button> */}
               <Components.Button.Button >AddLiqudity</Components.Button.Button>
               <Components.Display.ME size="1" />
               <Components.Button.Button>Trade</Components.Button.Button>
@@ -178,12 +404,12 @@ const PoolPage = ({} : PoolPage): JSX.Element => {
                 <Components.Display.FullRow>
                   <Components.Display.ContentBetween>
                     <div className="d-flex">
-                      <Components.Icons.TokenIcon src=""/>
+                      <Components.Icons.TokenIcon src={tokenIcon1}/>
                       <Components.Display.ME size="1" />
-                      <LeadText>Token1</LeadText>
+                      <LeadText>{tokenSymbol1}</LeadText>
                     </div>
                     <div className="">
-                      <LeadText>155.79m</LeadText>
+                      <LeadText>{reserved1}</LeadText>
                     </div>
                   </Components.Display.ContentBetween>
                 </Components.Display.FullRow>
@@ -192,12 +418,12 @@ const PoolPage = ({} : PoolPage): JSX.Element => {
                 <Components.Display.FullRow>
                   <Components.Display.ContentBetween>
                     <div className="d-flex">
-                      <Components.Icons.TokenIcon src=""/>
+                      <Components.Icons.TokenIcon src={tokenIcon2}/>
                       <Components.Display.ME size="1" />
-                      <LeadText>Token2</LeadText>
+                      <LeadText>{tokenSymbol2}</LeadText>
                     </div>
                     <div className="">
-                      <LeadText>1123.79m</LeadText>
+                      <LeadText>{reserved2}</LeadText>
                     </div>
                   </Components.Display.ContentBetween>
                 </Components.Display.FullRow>
@@ -207,14 +433,27 @@ const PoolPage = ({} : PoolPage): JSX.Element => {
 
               <div className="d-flex flex-column mt-3 ms-1">
                 <Text size={1.3}>TVL</Text>
-                <Components.Text.BoldText size={1.6}>$371.41m</Components.Text.BoldText>
-                <Components.Text.ColorText color="success" size={1}>2.14 %</Components.Text.ColorText>
+                <Components.Text.BoldText size={1.6}>{totalSupply}</Components.Text.BoldText>
+                <Components.Text.ColorText color={totalSupplyPercentage < 0 ? "danger" : "success"} size={1}>{totalSupplyPercentage.toFixed(3) + "1"} %</Components.Text.ColorText>
               </div>
 
               <div className="d-flex flex-column mt-3 ms-1">
-                <Text size={1.3}>Colume 24h</Text>
-                <Components.Text.BoldText size={1.6}>$71.41m</Components.Text.BoldText>
-                <Components.Text.ColorText color="danger" size={1}>38.2 %</Components.Text.ColorText>
+                <Text size={1.3}>Volume (24h)</Text>
+                <div className="d-flex">
+                  <Components.Icons.TokenIcon src={tokenIcon1}/>
+                  <Components.Text.BoldText size={1.6}> 
+                    {volume1}
+                  </Components.Text.BoldText>
+                </div>
+                <Components.Text.ColorText color={volumeDifference1 < 0 ? "danger" : "success"} size={1}>{volumeDifference1.toFixed(3) + "1"} %</Components.Text.ColorText>
+
+                <div className="d-flex">
+                  <Components.Icons.TokenIcon src={tokenIcon2}/>
+                  <Components.Text.BoldText size={1.6}>
+                    {volume2}
+                  </Components.Text.BoldText>
+                </div>
+                <Components.Text.ColorText color={volumeDifference2 < 0 ? "danger" : "success"} size={1}>{volumeDifference2.toFixed(3) + "1"} %</Components.Text.ColorText>
               </div>
               <div className="d-flex flex-column mt-3 ms-1">
                 <Text size={1.3}>24h Fees</Text>
