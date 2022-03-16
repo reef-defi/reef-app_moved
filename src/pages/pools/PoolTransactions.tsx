cd57d805c33932f7c56bd42a9d70fa4b25857767
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-03-16 20:29:28 +0100

    pool transactions now accept optional pool address, all other fields were removed

--- src/pages/pools/PoolTransactions.tsx
+++ src/pages/pools/PoolTransactions.tsx
@@ -3,6 +3,7 @@ import {Components} from "@reef-defi/react-lib";
 import {useSubscription, useQuery, gql} from "@apollo/client";
 import { formatAmount, shortAddress, formatAgoDate } from "../../utils/utils";
 import { innitialNetwork } from "../../environment";
+import { ContractData } from "./poolTypes";
 
 const {BoldText} = Components.Text;
 const {Loading} = Components.Loading;
@@ -11,11 +12,11 @@ type BasePoolTransactionTypes = "Swap" | "Mint" | "Burn"
 type TransactionTypes =  BasePoolTransactionTypes | "All"
 
 interface PoolTransactions {
-  address: string;
-  tokenSymbol1: string;
-  tokenSymbol2: string;
-  decimal1: number;
-  decimal2: number;
+  address?: string;
+  // tokenSymbol1: string;
+  // tokenSymbol2: string;
+  // decimal1: number;
+  // decimal2: number;
 }
 
 interface PoolTransaction {
@@ -27,6 +28,18 @@ interface PoolTransaction {
   to_address: string | null;
   timestamp: string;
   type: BasePoolTransactionTypes;
+  pool: {
+    token_contract_1: {
+      verified_contract: null | {
+        contract_data: ContractData
+      };
+    };
+    token_contract_2: {
+      verified_contract: null | {
+        contract_data: ContractData
+      };
+    };
+  };
   evm_event: {
     event: {
       extrinsic: {
@@ -47,7 +60,7 @@ interface PoolTransactionCount {
 type PoolTransactionQuery = { pool_event: PoolTransaction[] };
 
 interface BasicTransactionVar {
-  address: string;
+  address: { _ilike?: string };
   type: BasePoolTransactionTypes[]
 }
 interface TransactionVar extends BasicTransactionVar {
@@ -55,11 +68,11 @@ interface TransactionVar extends BasicTransactionVar {
 }
 
 const POOL_TRANSACTIONS_GQL = gql`
-subscription transactions($address: String!, $type: [pooltype!], $offset: Int!) {
+subscription transactions($address: String_comparison_exp!, $type: [pooltype!], $offset: Int!) {
   pool_event(
     order_by: { timestamp: desc }
     where: {
-      pool: { address: { _ilike: $address } }
+      pool: { address: $address }
       type: { _in: $type }
     }
     limit: 10
@@ -73,6 +86,18 @@ subscription transactions($address: String!, $type: [pooltype!], $offset: Int!) 
     to_address
     timestamp
     type
+    pool {
+      token_contract_2 {
+        verified_contract {
+          contract_data
+        }
+      }
+      token_contract_1 {
+        verified_contract {
+          contract_data
+        }
+      }
+    }
     evm_event {
       event {
         extrinsic {
@@ -86,10 +111,10 @@ subscription transactions($address: String!, $type: [pooltype!], $offset: Int!) 
 `;
 
 const POOL_TRANSACTION_COUNT_GQL = gql`
-subscription transaction_count($address: String!, $type: [pooltype]!) {
+subscription transaction_count($address: String_comparison_exp!, $type: [pooltype]!) {
   pool_event_aggregate( 
     where: {
-      pool: { address: { _ilike: $address } }
+      pool: { address: $address }
       type: { _in: $type }
     } 
   ) {
@@ -100,12 +125,12 @@ subscription transaction_count($address: String!, $type: [pooltype]!) {
 }
 `;
 
-const PoolTransactions = ({address, tokenSymbol1, tokenSymbol2, decimal1, decimal2} : PoolTransactions): JSX.Element => {
+const PoolTransactions = ({address} : PoolTransactions): JSX.Element => {
   const [pageIndex, setPageIndex] = useState(0);
   const [type, setType] = useState<TransactionTypes>("All");
   
   const variables: BasicTransactionVar = {
-    address,
+    address: address ? { _ilike: address } : {},
     type: (type === "All" ? ["Swap", "Mint", "Burn"] : [type])
   };
   const {loading: loadingTransactions, data: transactionData} = useSubscription<PoolTransactionQuery, TransactionVar>(
@@ -126,39 +151,46 @@ const PoolTransactions = ({address, tokenSymbol1, tokenSymbol2, decimal1, decima
   const nextPage = () => setPageIndex(Math.min(maxPage, pageIndex + 1))
   const prevPage = () => setPageIndex(Math.max(0, pageIndex - 1))
 
-  const description = (type: BasePoolTransactionTypes, amount_1: number) => {
+  const description = (type: BasePoolTransactionTypes, amount_1: number, tokenSymbol1: string, tokenSymbol2: string) => {
     switch (type) {
       case "Swap": return `${type} ${amount_1 > 0 ? tokenSymbol1 : tokenSymbol2} for ${amount_1 > 0 ? tokenSymbol2 : tokenSymbol1}`
       case "Burn": return `Remove ${tokenSymbol1} and ${tokenSymbol2}`;
       case "Mint": return `Add ${tokenSymbol1} and ${tokenSymbol2}`;
     }
   }
-  // TODO add liquidity signer is wrong!!!!!
+
   const transactionView = !loadingTransactions && transactionData
     ? transactionData.pool_event
-    .map(({amount_1, amount_2, timestamp, to_address, type, amount_in_1, amount_in_2, evm_event: {event: {extrinsic: {hash, signer}}}}, index) => (
-      <tr key={`transaction-${index}`}>
-        <td className="fs-5"><a href={`${innitialNetwork.reefscanFrontendUrl}/extrinsic/${hash}`}>{description(type, amount_1)}</a></td>
-        <td className="text-end fs-5 d-none d-md-table-cell d-lg-table-cell d-xl-table-cell">{formatAmount(amount_1 > 0 ? amount_1 : amount_in_1, decimal1)} {tokenSymbol1}</td>
-        <td className="text-end fs-5 d-none d-lg-table-cell d-xl-table-cell">{formatAmount(amount_2 > 0 ? amount_2 : amount_in_2, decimal2)} {tokenSymbol2}</td>
-        <td className="text-end fs-5 d-none d-xl-table-cell"><a href={`${innitialNetwork.reefscanFrontendUrl}/account/${to_address || signer}`}>{shortAddress(to_address || signer)}</a></td>
-        <td className="text-end pe-4 fs-5">{formatAgoDate(timestamp)}</td>
-      </tr>
-    ))
+    .map(({amount_1, amount_2, timestamp, to_address, type, amount_in_1, amount_in_2, evm_event: {event: {extrinsic: {hash, signer}}}, pool: {token_contract_1, token_contract_2}}, index) => {
+      const symbol1 = token_contract_1.verified_contract?.contract_data.symbol || "?";
+      const decimal1 =token_contract_1.verified_contract?.contract_data.decimals || 18; 
+      const symbol2 = token_contract_2.verified_contract?.contract_data.symbol || "?";
+      const decimal2 =token_contract_2.verified_contract?.contract_data.decimals || 18; 
+
+      return (
+        <tr key={`transaction-${index}`}>
+          <td className="fs-5"><a href={`${innitialNetwork.reefscanFrontendUrl}/extrinsic/${hash}`}>{description(type, amount_1, symbol1, symbol2)}</a></td>
+          <td className="text-end fs-5 d-none d-md-table-cell d-lg-table-cell d-xl-table-cell">{formatAmount(amount_1 > 0 ? amount_1 : amount_in_1, decimal1)} {symbol1}</td>
+          <td className="text-end fs-5 d-none d-lg-table-cell d-xl-table-cell">{formatAmount(amount_2 > 0 ? amount_2 : amount_in_2, decimal2)} {symbol2}</td>
+          <td className="text-end fs-5 d-none d-xl-table-cell"><a href={`${innitialNetwork.reefscanFrontendUrl}/account/${to_address || signer}`}>{shortAddress(to_address || signer)}</a></td>
+          <td className="text-end pe-4 fs-5">{formatAgoDate(timestamp)}</td>
+        </tr>
+      );
+    })
     : [];
 
   return (
     <>
       <Components.Display.MT size="4" />
       <BoldText size={1.6}>Transactions</BoldText>
-      <Components.Display.MT size="4" />
+      <Components.Display.MT size="2" />
       
       <Components.Card.Card>
         {
           loadingTransactions
             ? <Loading />
             : <>
-              <table className="table">
+              <table className="table table-hover">
                 <thead>
                   <tr>
                     <th scope="col">
