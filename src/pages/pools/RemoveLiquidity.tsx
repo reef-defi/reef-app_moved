a43e02f264e89c44ce5bdda27ba457b5938808bc
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-04-19 18:26:46 +0200

    remove fixed

--- src/pages/pools/RemoveLiquidity.tsx
+++ src/pages/pools/RemoveLiquidity.tsx
@@ -4,6 +4,7 @@ import {
 } from '@reef-defi/react-lib';
 import { Redirect, useHistory, useParams } from 'react-router-dom';
 import { POOLS_URL } from '../../urls';
+import { useTokensFinder } from '../../hooks/useTokensFinder';
 
 const { RemoveLiquidityComponent } = Components;
 
@@ -12,18 +13,15 @@ interface UrlParams {
   address2: string;
 }
 
-const findToken = (address: string, tokens: Token[] = []): Token|undefined => tokens.find((token) => token.address === address);
 
 const RemoveLiquidity = (): JSX.Element => {
   const history = useHistory();
   const { address1, address2 } = useParams<UrlParams>();
   const tokens: Token[]|undefined = hooks.useObservableState(appState.allAvailableSignerTokens$);
   const network: Network|undefined = hooks.useObservableState(appState.selectedNetworkSubj);
-
   const signer: ReefSigner|undefined = hooks.useObservableState(appState.selectedSigner$);
-  const token1 = findToken(address1, tokens);
-  const token2 = findToken(address2, tokens);
 
+  const [token1, token2, state] = useTokensFinder({ address1, address2, tokens, signer});
   const back = (): void => history.push(POOLS_URL);
   /* const onRemoveLiqUpdate = (txState: utils.TxStatusUpdate): void => {
     const updateTypes = [UpdateDataType.ACCOUNT_NATIVE_BALANCE, UpdateDataType.ACCOUNT_TOKENS];
@@ -33,9 +31,7 @@ const RemoveLiquidity = (): JSX.Element => {
 
   return (
     <>
-      {(!token1 || !token2)
-      && (<Redirect to={POOLS_URL} />)}
-      { token1 && token2 && network
+      {network && signer && state === 'Success'
      && (
      <RemoveLiquidityComponent
        token1={token1}
