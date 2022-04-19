cdff5904156fa167dc933b81ad5ec35bd5e36eeb
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-04-19 18:46:28 +0200

    remove liquidity fixed

--- src/pages/pools/RemoveLiquidity.tsx
+++ src/pages/pools/RemoveLiquidity.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 import {
-  Components, Token, appState, hooks, Network, ReefSigner,
-} from '@reef-defi/react-lib';
-import { Redirect, useHistory, useParams } from 'react-router-dom';
-import { POOLS_URL } from '../../urls';
-import { useTokensFinder } from '../../hooks/useTokensFinder';
+  Components,
+  Token,
+  appState,
+  hooks,
+  Network,
+  ReefSigner,
+} from "@reef-defi/react-lib";
+import { Redirect, useHistory, useParams } from "react-router-dom";
+import { POOLS_URL } from "../../urls";
+import { useTokensFinder } from "../../hooks/useTokensFinder";
 
 const { RemoveLiquidityComponent } = Components;
 
@@ -13,38 +18,45 @@ interface UrlParams {
   address2: string;
 }
 
-
 const RemoveLiquidity = (): JSX.Element => {
   const history = useHistory();
   const { address1, address2 } = useParams<UrlParams>();
-  const tokens: Token[]|undefined = hooks.useObservableState(appState.allAvailableSignerTokens$);
-  const network: Network|undefined = hooks.useObservableState(appState.selectedNetworkSubj);
-  const signer: ReefSigner|undefined = hooks.useObservableState(appState.selectedSigner$);
+  const tokens: Token[] | undefined = hooks.useObservableState(
+    appState.allAvailableSignerTokens$
+  );
+  const network: Network | undefined = hooks.useObservableState(
+    appState.selectedNetworkSubj
+  );
+  const signer: ReefSigner | undefined = hooks.useObservableState(
+    appState.selectedSigner$
+  );
 
-  const [token1, token2, state] = useTokensFinder({ address1, address2, tokens, signer});
+  const [token1, token2, state] = useTokensFinder({
+    address1,
+    address2,
+    tokens,
+    signer,
+  });
   const back = (): void => history.push(POOLS_URL);
-  /* const onRemoveLiqUpdate = (txState: utils.TxStatusUpdate): void => {
-    const updateTypes = [UpdateDataType.ACCOUNT_NATIVE_BALANCE, UpdateDataType.ACCOUNT_TOKENS];
-    const updateActions: UpdateAction[] = createUpdateActions(updateTypes, txState.addresses);
-    onTxUpdateResetSigners(txState, updateActions);
-  }; */
+
+  // Redirecting to pools page if any of tokens is empty on success
+  if (state === 'Success' && (token1.isEmpty || token2.isEmpty)) {
+    return <Redirect to={POOLS_URL} />
+  }
+
+  if (state !== 'Success') {
+    return <div>Loading...</div>
+  }
 
   return (
-    <>
-      {network && signer && state === 'Success'
-     && (
-     <RemoveLiquidityComponent
-       token1={token1}
-       token2={token2}
-       signer={signer}
-       network={network}
-       back={back}
-       // onTxUpdate={onRemoveLiqUpdate}
-     />
-     )}
-
-    </>
-  );
+    <RemoveLiquidityComponent
+      token1={token1}
+      token2={token2}
+      signer={signer}
+      network={network}
+      back={back}
+    />
+  )
 };
 
 export default RemoveLiquidity;
