d966e351e4459918c5b63189511e7c78bf54a0a9
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-04-21 11:05:28 +0200

    Toast container added in base of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  appState, graphql, hooks, ReefSigner,
+  appState, defaultOptions, graphql, hooks, ReefSigner,
 } from '@reef-defi/react-lib';
 import { ApolloProvider, ApolloClient } from '@apollo/client';
 import ContentRouter from './pages/ContentRouter';
@@ -11,7 +11,9 @@ import NoAccount from './pages/error/NoAccount';
 import { innitialNetwork } from './environment';
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
-
+import { useHistory } from 'react-router-dom';
+import OptionContext from "./context/OptionContext";
+import { notify } from './utils/utils';
 const App = (): JSX.Element => {
   const { provider, loading, error } = hooks.useInitReefState(
     'Reef Wallet App',
@@ -19,40 +21,44 @@ const App = (): JSX.Element => {
       network: innitialNetwork,
     },
   );
-
+  const history = useHistory();
   const currentSigner: ReefSigner|undefined = hooks.useObservableState(appState.selectedSigner$);
   const apollo: ApolloClient<any>|undefined = hooks.useObservableState(graphql.apolloClientInstance$);
   hooks.useBindEvmAddressAlert(currentSigner, provider);
+
+  
 
   return (
     <>
       {apollo
     && (
     <ApolloProvider client={apollo}>
-      <div className="App d-flex w-100 h-100">
-        <div className="w-100 main-content">
-          {!loading && !error && (
-          <>
-            <Nav display={!loading && !error} />
-            <ContentRouter />            
-            </>
-          )}
+      <OptionContext.Provider value={{ ...defaultOptions, back: history.goBack, notify }}>
+        <div className="App d-flex w-100 h-100">
+          <div className="w-100 main-content">
+            {!loading && !error && (
+            <>
+              <Nav display={!loading && !error} />
+              <ContentRouter />            
+              </>
+            )}
 
-          {error?.code === 1 && <NoExtension />}
-          {error?.code === 2 && <NoAccount />}
-          <ToastContainer
-            draggable
-            newestOnTop
-            closeOnClick
-            hideProgressBar
-            position={toast.POSITION.BOTTOM_LEFT}
-            autoClose={5000}
-            rtl={false}
-            pauseOnFocusLoss={false}
-            pauseOnHover={false} />
+            {error?.code === 1 && <NoExtension />}
+            {error?.code === 2 && <NoAccount />}
+            <ToastContainer
+              draggable
+              newestOnTop
+              closeOnClick
+              hideProgressBar
+              position={toast.POSITION.BOTTOM_LEFT}
+              autoClose={5000}
+              rtl={false}
+              pauseOnFocusLoss={false}
+              pauseOnHover={false} />
 
+          </div>
         </div>
-      </div>
+      </OptionContext.Provider>
     </ApolloProvider>
     )}
     </>
