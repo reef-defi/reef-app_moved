63d74d74a5eb61b5de6bc69d16f36981817b6bac
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-04-21 20:33:24 +0200

    transfer switched with send component

--- src/pages/transfer/Transfer.tsx
+++ src/pages/transfer/Transfer.tsx
@@ -3,44 +3,29 @@ import {
   Components,
   hooks,
   ReefSigner,
-  Token,
-  TokenWithAmount,
-  utils as reefUtils,
 } from '@reef-defi/react-lib';
-import React from 'react';
-import { Provider } from '@reef-defi/evm-provider';
+import React, { useContext } from 'react';
+import TokenContext from '../../context/TokenContext';
+import { notify } from '../../utils/utils';
+import { Provider } from "@reef-defi/evm-provider";
 
-const {
-  isDataSet,
-  getData,
-  DataProgress,
-} = reefUtils;
-
-const {
-  Loading, TransferComponent,
-} = Components;
-
-/* const onTransferTxUpdate = (txState: reefUtils.TxStatusUpdate): void => {
-
-  const updateTypes = [UpdateDataType.ACCOUNT_NATIVE_BALANCE];
-  if (txState.txTypeEvm) {
-    updateTypes.push(UpdateDataType.ACCOUNT_TOKENS);
-  }
-  const updateActions: UpdateAction[] = createUpdateActions(updateTypes, txState.addresses);
-  onTxUpdateResetSigners(txState, updateActions);
-}; */
+const { Send } = Components;
 
 export const Transfer = (): JSX.Element => {
   const provider: Provider|undefined = hooks.useObservableState(appState.providerSubj);
   const accounts: ReefSigner[]|undefined = hooks.useObservableState(appState.signers$);
   const selectedSigner: ReefSigner|undefined = hooks.useObservableState(appState.selectedSigner$);
-  const signerTokenBalances: TokenWithAmount[]|undefined = hooks.useObservableState(appState.tokenPrices$);
+  const tokens = useContext(TokenContext);
 
-  return (
-    <>
-      {!signerTokenBalances && <Loading.Loading />}
-      { provider && signerTokenBalances && selectedSigner
-      && <TransferComponent tokens={signerTokenBalances as Token[]} from={selectedSigner} provider={provider} accounts={accounts || []} currentAccount={selectedSigner} />}
-    </>
-  );
+  if (!accounts || !selectedSigner || !provider) {
+    return <div />;
+  }
+
+  return <Send 
+    notify={notify}
+    accounts={accounts}
+    provider={provider}
+    signer={selectedSigner}
+    tokens={tokens.filter(({balance}) => balance.gt(0))}
+  />
 };
