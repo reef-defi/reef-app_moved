eff4e0cba5dfac5b30968d71892ebd7cd3576100
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-04-21 14:34:26 +0200

    app loads all validated tokens with signer balances and creates their context

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  appState, defaultOptions, graphql, hooks, ReefSigner,
+  appState, defaultOptions, graphql, hooks, ReefSigner, api
 } from '@reef-defi/react-lib';
 import { ApolloProvider, ApolloClient } from '@apollo/client';
 import ContentRouter from './pages/ContentRouter';
 import Nav from './common/Nav';
 import NoExtension from './pages/error/NoExtension';
 import NoAccount from './pages/error/NoAccount';
-// import {innitialNetwork} from './environment';
 import { innitialNetwork } from './environment';
 import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
 import { useHistory } from 'react-router-dom';
 import OptionContext from "./context/OptionContext";
+import TokenContext from "./context/TokenContext";
 import { notify } from './utils/utils';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 const App = (): JSX.Element => {
   const { provider, loading, error } = hooks.useInitReefState(
     'Reef Wallet App',
@@ -26,39 +28,46 @@ const App = (): JSX.Element => {
   const apollo: ApolloClient<any>|undefined = hooks.useObservableState(graphql.apolloClientInstance$);
   hooks.useBindEvmAddressAlert(currentSigner, provider);
 
-  
+  const {value: tokens} = hooks.useLoader(
+    api.loadVerifiedTokens,
+    [currentSigner?.address, apollo],
+    [],
+    [apollo, currentSigner]
+  );
 
   return (
     <>
       {apollo
     && (
     <ApolloProvider client={apollo}>
-      <OptionContext.Provider value={{ ...defaultOptions, back: history.goBack, notify }}>
-        <div className="App d-flex w-100 h-100">
-          <div className="w-100 main-content">
-            {!loading && !error && (
-            <>
-              <Nav display={!loading && !error} />
-              <ContentRouter />            
-              </>
-            )}
+      <TokenContext.Provider value={tokens}>
+        <OptionContext.Provider value={{ ...defaultOptions, back: history.goBack, notify }}>
+          <div className="App d-flex w-100 h-100">
+            <div className="w-100 main-content">
+              {!loading && !error && (
+              <>
+                <Nav display={!loading && !error} />
+                <ContentRouter />            
+                </>
+              )}
 
-            {error?.code === 1 && <NoExtension />}
-            {error?.code === 2 && <NoAccount />}
-            <ToastContainer
-              draggable
-              newestOnTop
-              closeOnClick
-              hideProgressBar
-              position={toast.POSITION.BOTTOM_LEFT}
-              autoClose={5000}
-              rtl={false}
-              pauseOnFocusLoss={false}
-              pauseOnHover={false} />
+              {error?.code === 1 && <NoExtension />}
+              {error?.code === 2 && <NoAccount />}
+              <ToastContainer
+                draggable
+                newestOnTop
+                closeOnClick
+                hideProgressBar
+                position={toast.POSITION.BOTTOM_LEFT}
+                autoClose={5000}
+                rtl={false}
+                pauseOnFocusLoss={false}
+                pauseOnHover={false} />
 
+            </div>
           </div>
-        </div>
-      </OptionContext.Provider>
+        </OptionContext.Provider>
+      </TokenContext.Provider>
     </ApolloProvider>
     )}
     </>
