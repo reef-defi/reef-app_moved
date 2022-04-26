59f8cc91ccdfad633a23335dacf847691f108339
Samo Šlag <samo.slag@gmail.com>
2022-04-26 14:14:21 +0200

    Create NFT component
    Create NFT component with image preloading while displaying loading animation

--- src/pages/dashboard/Nfts.tsx
+++ src/pages/dashboard/Nfts.tsx
@@ -4,6 +4,8 @@ import { TokenPill } from './TokenPill';
 import './Nfts.css';
 import {TokenActivityItem, TokenActivityType} from "./TokenActivityItem";
 
+import NFT from "./NFT"
+
 const { isDataSet, DataProgress } = utils;
 
 const placeholderImage = 'https://cryptotelegram.com/wp-content/uploads/2021/04/reef-crypto-explained.jpg';
@@ -27,17 +29,8 @@ export const Nfts = ({tokens}: Nfts): JSX.Element => {
 
         {!!tokens && !!tokens.length && (
             <div className="nfts__container">
-                {tokens.map((t, i) => (
-                    <div className="nfts__item" key={i}>
-                        <div
-                            className="nfts__item-image"
-                            style={{ backgroundImage: `url(${t.iconUrl})` }}
-                        />
-                        <div className="nfts__item-info">
-                            <div className="nfts__item-name">{t.name}</div>
-                            <div className='nfts__item-balance'>{t.balance}</div>
-                        </div>
-                    </div>
+                {tokens.map((token, i) => (
+                    <NFT data={token} key={i}/>
                 ))}
             </div>
         )}
