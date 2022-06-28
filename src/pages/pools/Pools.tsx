2cba0458bcf2aba658bfc5ca0cc7c67b1ac47c89
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-06-28 13:34:49 +0200

    pools

--- src/pages/pools/Pools.tsx
+++ src/pages/pools/Pools.tsx
@@ -12,13 +12,14 @@ import { hooks } from '@reef-defi/react-lib';
 const Pools = (): JSX.Element => {
   const tokenPrices = useContext(TokenPricesContext);
   const totalLiquidity = hooks.useTotalSupply(tokenPrices);
+
   return (
     <div className="pools">
       <Uik.Container className="pools__top">
         <div className="pools__total">
           <Uik.Text type="lead">Total Supply</Uik.Text>
           <div className="pools__total-amount">
-            <Uik.Text type="headline">$ {totalLiquidity}</Uik.Text>
+            <Uik.Text type="headline">$ {totalLiquidity.split('.')[0]}</Uik.Text>
             <Uik.Trend
               type="good"
               text="$1,650K"
@@ -26,16 +27,13 @@ const Pools = (): JSX.Element => {
             />
           </div>
         </div>
-
         <Uik.Button
           icon={faRightLeft}
           text="Show Transactions"
         />
       </Uik.Container>
-
       <MyPoolsList />
       <PoolsList />
-
     </div>
   );
 }
