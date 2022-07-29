4eaa90419959182e8f8eca5e46b2a3f9c821f6bc
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-07-29 20:00:38 +0200

    contexts repaired in content router

--- src/pages/ContentRouter.tsx
+++ src/pages/ContentRouter.tsx
@@ -27,17 +27,18 @@ import { Bonds } from './bonds/Bonds';
 import PoolContext from '../context/PoolContext';
 import TokenContext from '../context/TokenContext';
 import TokenPrices from '../context/TokenPricesContext';
+import NftContext from '../context/NftContext';
 import Bind from './bind/Bind';
 import Pool from './pools/Pool/Pool';
 
 const ContentRouter = (): JSX.Element => {
   const currentSigner: ReefSigner|undefined|null = hooks.useObservableState(appState.selectedSigner$);
-  const apollo: ApolloClient<any>|undefined = hooks.useObservableState(graphql.apolloClientInstance$);
   const reefPrice = hooks.useObservableState(appState.reefPrice$);
   // Its not appropriet to have token state in this component, but the problem was apollo client.
   // Once its decared properlly in App move TokenContext in the parent component (App.tsx)
 
-  const tokens = hooks.useAllTokens(currentSigner?.address, apollo);
+  const [tokens, tokenLoading] = hooks.useAllTokens(currentSigner?.address);
+  const [nfts, nftsLoading] = hooks.useAllNfts(currentSigner?.address);
   const pools = hooks.useAllPools();
   const tokenPrices = useMemo(
     () => hooks.estimatePrice(tokens, pools, reefPrice || 0),
@@ -46,25 +47,27 @@ const ContentRouter = (): JSX.Element => {
 
   return (
     <div className="content">
-      <TokenContext.Provider value={tokens}>
-        <PoolContext.Provider value={pools}>
-          <TokenPrices.Provider value={tokenPrices}>
-            <Switch>
-              <Route path={SPECIFIED_SWAP_URL} component={Swap} />
-              <Route exact path={POOLS_URL} component={Pools} />
-              <Route exact path={DASHBOARD_URL} component={Dashboard} />
-              <Route path={ADD_LIQUIDITY_URL} component={AddLiqudity} />
-              <Route exact path={ADD_LIQUIDITY_URL} component={AddLiqudity} />
-              <Route path={POOL_CHART_URL} component={Pool} />
-              <Route path={REMOVE_LIQUIDITY_URL} component={RemoveLiquidity} />
-              <Route exact path={TRANSFER_TOKEN} component={Transfer} />
-              <Route exact path={CREATE_ERC20_TOKEN_URL} component={Creator} />
-              <Route exact path={BONDS_URL} component={Bonds} />
-              <Route path={BIND_URL} component={Bind} />
-              <Route path="/" render={() => (<Redirect to={DASHBOARD_URL} />)} />
-            </Switch>
-          </TokenPrices.Provider>
-        </PoolContext.Provider>
+      <TokenContext.Provider value={{tokens, loading: tokenLoading}}>
+        <NftContext.Provider value={{nfts, loading: nftsLoading}}>
+          <PoolContext.Provider value={pools}>
+            <TokenPrices.Provider value={tokenPrices}>
+              <Switch>
+                <Route path={SPECIFIED_SWAP_URL} component={Swap} />
+                <Route exact path={POOLS_URL} component={Pools} />
+                <Route exact path={DASHBOARD_URL} component={Dashboard} />
+                <Route path={ADD_LIQUIDITY_URL} component={AddLiqudity} />
+                <Route exact path={ADD_LIQUIDITY_URL} component={AddLiqudity} />
+                <Route path={POOL_CHART_URL} component={Pool} />
+                <Route path={REMOVE_LIQUIDITY_URL} component={RemoveLiquidity} />
+                <Route exact path={TRANSFER_TOKEN} component={Transfer} />
+                <Route exact path={CREATE_ERC20_TOKEN_URL} component={Creator} />
+                <Route exact path={BONDS_URL} component={Bonds} />
+                <Route path={BIND_URL} component={Bind} />
+                <Route path="/" render={() => (<Redirect to={DASHBOARD_URL} />)} />
+              </Switch>
+            </TokenPrices.Provider>
+          </PoolContext.Provider>
+        </NftContext.Provider>
       </TokenContext.Provider>
     </div>
   );
