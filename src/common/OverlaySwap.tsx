cc46d03ca371047313e6c7527db0b90a4e1f26d2
Ziga Franko G <frenkie@Zigas-MacBook-Pro.local>
2022-08-11 23:33:17 +0200

    trade popup

---
+++ src/common/OverlaySwap.tsx
@@ -0,0 +1,82 @@
+import { appState, Components, hooks, store, Token, utils } from '@reef-defi/react-lib';
+import React, { useContext, useReducer, useState } from 'react';
+import TokenContext from '../context/TokenContext';
+import TokenPricesContext from '../context/TokenPricesContext';
+import { notify } from '../utils/utils';
+import OverlayAction from './OverlayAction';
+
+const {Trade} = Components;
+
+export interface OverlaySwap {
+  isOpen: boolean,
+  onClose?: () => any
+}
+
+const OverlaySwap = ({
+  isOpen,
+  onClose,
+}: OverlaySwap): JSX.Element => {
+  const [address1, setAddress1] = useState(utils.REEF_ADDRESS);
+  const [address2, setAddress2] = useState("0x");
+  const {tokens} = useContext(TokenContext);
+  const tokenPrices = useContext(TokenPricesContext);
+
+  const network = hooks.useObservableState(appState.currentNetwork$);
+  const signer = hooks.useObservableState(appState.selectedSigner$);
+  // Trade
+  const [tradeState, tradeDispatch] = useReducer(
+    store.swapReducer,
+    store.initialSwapState,
+  );
+
+
+  hooks.useSwapState({
+    address1,
+    address2,
+    dispatch: tradeDispatch,
+    state: tradeState,
+    tokenPrices,
+    tokens,
+    account: signer || undefined,
+    network,
+  });
+
+  const onSwap = hooks.onSwap({
+    state: tradeState,
+    network,
+    account: signer || undefined,
+    dispatch: tradeDispatch,
+    notify,
+    updateTokenState: async () => {}, // eslint-disable-line
+  });
+  const onSwitch = (): void => {
+    tradeDispatch(store.switchTokensAction());
+    tradeDispatch(store.setPercentageAction(0));
+    tradeDispatch(store.clearTokenAmountsAction());
+  };
+
+  return (
+    <OverlayAction
+      isOpen={isOpen}
+      title="Swap"
+      onClose={onClose}
+    >(
+      <Trade
+        tokens={tokens}
+        state={tradeState}
+        actions={{
+          onSwap,
+          onSwitch,
+          setPercentage: (amount: number) => tradeDispatch(store.setPercentageAction(amount)),
+          setToken1Amount: (amount: string): void => tradeDispatch(store.setToken1AmountAction(amount)),
+          setToken2Amount: (amount: string): void => tradeDispatch(store.setToken2AmountAction(amount)),
+          selectToken1: (token: Token): void => setAddress1(token.address),
+          selectToken2: (token: Token): void => setAddress2(token.address),
+        }}
+      />
+    );
+    </OverlayAction>
+  );
+}
+
+export default OverlaySwap;
