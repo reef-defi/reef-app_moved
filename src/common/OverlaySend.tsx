2744628ada937a85ff7b430510300ae74aec7140
Ziga Franko G <frenkie@Zigas-MacBook-Pro.local>
2022-08-12 13:20:07 +0200

    overlay send

---
+++ src/common/OverlaySend.tsx
@@ -0,0 +1,42 @@
+import { appState, Components, hooks } from '@reef-defi/react-lib';
+import React, { useContext } from 'react';
+import TokenContext from '../context/TokenContext';
+import { notify } from '../utils/utils';
+
+const {Send, OverlayAction} = Components;
+
+export interface OverlaySend {
+  isOpen: boolean,
+  onClose?: () => any
+}
+
+const OverlaySend = ({
+  isOpen,
+  onClose,
+}: OverlaySend): JSX.Element => {
+  const {tokens} = useContext(TokenContext);
+
+  const signer = hooks.useObservableState(appState.selectedSigner$);
+  const accounts = hooks.useObservableState(appState.accountsSubj);
+  const provider = hooks.useObservableState(appState.currentProvider$);
+
+  return (
+    <OverlayAction
+      isOpen={isOpen}
+      title="Swap"
+      onClose={onClose}
+    > 
+      { provider && signer &&
+        <Send
+          accounts={accounts || []}
+          notify={notify}
+          provider={provider}
+          signer={signer}
+          tokens={tokens}
+        />
+      }
+    </OverlayAction>
+  );
+}
+
+export default OverlaySend;
