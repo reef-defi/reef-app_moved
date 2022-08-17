e9628ce269cac42f6920c55e6c6e4d1a1110c0cc
Samo Šlag <samo.slag@gmail.com>
2022-08-17 16:11:37 +0200

    Recreate Balance on Dashboard with option to hide value

--- src/pages/dashboard/Balance.tsx
+++ src/pages/dashboard/Balance.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import Uik from '@reef-defi/ui-kit';
+import React, { useState, useMemo } from 'react';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { toCurrencyFormat } from '../../utils/utils';
 
 interface Balance {
   balance: number;
   loading: boolean;
+  className?: string;
 }
 
 export const Loading = (): JSX.Element => (
-  <span className="dashboard-balance-loading dashboard_balance-txt title-font text-bold text-color-dark-accent">
-    <div className="dashboard-balance-loading__container">
+  <span className="dashboard__balance-loading">
+    <div className="dashboard__balance-loading__container">
       <span>$</span>
       <span>.</span>
       <span>.</span>
@@ -17,18 +20,67 @@ export const Loading = (): JSX.Element => (
   </span>
 );
 
-export const Balance = ({ balance, loading }:Balance): JSX.Element => (
-  <div className="dashboard_balance col-12 col-md-6">
-    <div>
-      <h5 className="text-semi-bold">Balance</h5>
+export const Balance = ({
+  balance,
+  loading,
+  className,
+}: Balance): JSX.Element => {
+  const [isHidden, setHidden] = useState(false);
+
+  const toggleHidden = (): boolean => {
+    const value = !isHidden;
+    setHidden(value);
+    return value;
+  };
+
+  const getBalance = useMemo((): string => toCurrencyFormat(balance as number, { maximumFractionDigits: balance < 10000 ? 2 : 0 }), [balance]);
+
+  return (
+    <div className={`
+      dashboard__balance
+      ${className || ''}
+    `}
+    >
+      <div className="dashboard__balance-label">
+        <Uik.Text type="lead">Balance</Uik.Text>
+        <button
+          key={String(isHidden)}
+          type="button"
+          className={`
+              dashboard__balance-hide-btn
+              ${isHidden ? 'dashboard__balance-hide-btn--hidden' : ''}
+            `}
+          onClick={toggleHidden}
+        >
+          <Uik.Icon icon={isHidden ? faEyeSlash : faEye} />
+        </button>
+      </div>
+      {
+        loading ? <Loading />
+          : (
+            <div
+              className={`
+                dashboard__balance-value
+                ${isHidden ? 'dashboard__balance-value--hidden' : ''}
+              `}
+            >
+              {
+                isHidden
+                  ? (
+                    <>
+                      $
+                      <div />
+                      <div />
+                      <div />
+                      <div />
+                      <div />
+                    </>
+                  )
+                  : getBalance
+              }
+            </div>
+          )
+      }
     </div>
-    <div>
-      {!loading && (
-        <span className="dashboard_balance-txt title-font text-bold text-color-dark-accent">
-          {toCurrencyFormat(balance as number, { maximumFractionDigits: balance < 10000 ? 2 : 0 })}
-        </span>
-      )}
-      {loading && <Loading />}
-    </div>
-  </div>
-);
+  );
+};
