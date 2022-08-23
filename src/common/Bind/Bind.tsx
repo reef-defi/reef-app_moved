2476fe9d9c76ef793efb9382b6b332f3ced18078
Samo <samo.slag@gmail.com>
2022-08-23 21:16:33 +0200

    Add current Bind component to bind popup

--- src/common/Bind/Bind.tsx
+++ src/common/Bind/Bind.tsx
@@ -1,11 +1,49 @@
 import React, { useMemo, useState, useEffect } from 'react';
-import { appState, hooks, ReefSigner } from '@reef-defi/react-lib';
+import {
+  appState, hooks, ReefSigner, Components,
+} from '@reef-defi/react-lib';
 import { faLink } from '@fortawesome/free-solid-svg-icons';
 import Uik from '@reef-defi/ui-kit';
 import './bind.css';
+import { TxStatusUpdate } from '@reef-defi/react-lib/dist/utils';
+
+const { EvmBindComponent } = Components;
 
 const Bind = (): JSX.Element => {
   const selectedSigner: ReefSigner|undefined | null = hooks.useObservableState(appState.selectedSigner$);
+  const accounts: ReefSigner[] | undefined | null = hooks.useObservableState(appState.signers$);
+
+  const onTxUpdate = (state: TxStatusUpdate): void => {
+    let updateActions: appState.UpdateAction[] = [];
+
+    if (state.componentTxType === Components.EvmBindComponentTxType.BIND) {
+      // bind
+      if (state.addresses && state.addresses.length) {
+        state.addresses.forEach((address) => {
+          updateActions.push({
+            address,
+            type: appState.UpdateDataType.ACCOUNT_EVM_BINDING,
+          } as appState.UpdateAction);
+          updateActions.push({
+            address,
+            type: appState.UpdateDataType.ACCOUNT_NATIVE_BALANCE,
+          } as appState.UpdateAction);
+        });
+      } else {
+        updateActions = [{ type: appState.UpdateDataType.ACCOUNT_EVM_BINDING }, { type: appState.UpdateDataType.ACCOUNT_NATIVE_BALANCE }];
+      }
+    } else {
+      // transaction
+      updateActions = state.addresses && state.addresses.length
+        ? state.addresses.map((address) => ({
+          address,
+          type: appState.UpdateDataType.ACCOUNT_NATIVE_BALANCE,
+        } as appState.UpdateAction))
+        : [{ type: appState.UpdateDataType.ACCOUNT_NATIVE_BALANCE }];
+    }
+
+    appState.onTxUpdateResetSigners(state, updateActions);
+  };
 
   const isBound = useMemo(() => !selectedSigner || selectedSigner?.isEvmClaimed, [selectedSigner]);
 
@@ -15,7 +53,7 @@ const Bind = (): JSX.Element => {
     if (!isBound) setOpen(true);
   }, [isBound]);
 
-  if (isBound) return <div />;
+  if (isBound || !selectedSigner || !accounts) return <div />;
 
   return (
     <div className="evm-bind">
@@ -24,7 +62,11 @@ const Bind = (): JSX.Element => {
         isOpen={isOpen}
         onClose={() => setOpen(false)}
       >
-        ...
+        <EvmBindComponent
+          bindSigner={selectedSigner}
+          onTxUpdate={onTxUpdate}
+          signers={accounts}
+        />
       </Uik.Modal>
 
       {
