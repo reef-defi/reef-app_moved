935f320b17fc962094ea165faee4856f14e86762
Samo Šlag <samo.slag@gmail.com>
2022-08-31 17:44:23 +0200

    Add Open Explorer button to Activity

--- src/pages/dashboard/Activity/Activity.tsx
+++ src/pages/dashboard/Activity/Activity.tsx
@@ -1,10 +1,11 @@
 import {
-  appState, createEmptyTokenWithAmount, hooks,
+  appState, createEmptyTokenWithAmount, hooks, ReefSigner, Network
 } from '@reef-defi/react-lib';
 import Uik from '@reef-defi/ui-kit';
 import React from 'react';
 import './activity.css';
 import ActivityItem, { Skeleton } from './ActivityItem';
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons"
 
 const noActivityTokenDisplay = createEmptyTokenWithAmount();
 noActivityTokenDisplay.address = '0x';
@@ -13,10 +14,26 @@ noActivityTokenDisplay.name = 'No account history yet.';
 
 export const Activity = (): JSX.Element => {
   const transfers = hooks.useObservableState(appState.transferHistory$);
+  
+  const signer: ReefSigner | undefined |null = hooks.useObservableState(appState.selectedSigner$);
+  const network: Network|undefined = hooks.useObservableState(appState.currentNetwork$);
+
+  console.log(signer)
 
   return (
     <div className="token-activity activity">
-      <Uik.Text type="title" text="Activity" className="activity__title" />
+      <div className='activity__head'>
+        <Uik.Text type="title" text="Activity" className="activity__title" />
+        {
+          !!signer?.address && !!network?.reefscanUrl &&
+          <Uik.Button
+            size="small"
+            icon={faArrowUpRightFromSquare}
+            text="Open Explorer"
+            onClick={() => window.open(`${network?.reefscanUrl}/account/${signer.address}`)}
+          />
+        }
+      </div>
 
       <div className={`col-12 card  ${transfers?.length ? 'card-bg-light' : ''}`}>
         {!!transfers && !transfers.length && <div className="no-token-activity">No recent transfer activity.</div>}
