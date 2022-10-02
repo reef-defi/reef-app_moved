59c94c3d67ddc812be5fe58106774d35f6ec5a79
Metod Ribič <metod.ribic@gmail.com>
2022-10-02 21:19:48 +0200

    fix: show corresponding logo based on the selected network

--- src/common/Nav.tsx
+++ src/common/Nav.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo, useContext } from 'react';
 import {
-  Components, appState, hooks, ReefSigner, Network,
+  availableNetworks, Components, appState, hooks, ReefSigner, Network,
 } from '@reef-defi/react-lib';
 import './Nav.css';
 import { Link, useHistory, useLocation } from 'react-router-dom';
@@ -24,6 +24,7 @@ const Nav = ({ display }: Nav): JSX.Element => {
   const signer: ReefSigner|undefined|null = hooks.useObservableState(appState.selectedSigner$);
   const accounts: ReefSigner[]|undefined|null = hooks.useObservableState(appState.signers$);
   const network: Network|undefined = hooks.useObservableState(appState.currentNetwork$);
+  const mainnetSelected = network == null || network?.rpcUrl === availableNetworks.mainnet.rpcUrl;
   const menuItems = [
     { title: 'Dashboard', url: DASHBOARD_URL },
     { title: 'Pools', url: POOLS_URL },
@@ -78,7 +79,7 @@ const Nav = ({ display }: Nav): JSX.Element => {
     <div className="nav-content navigation d-flex d-flex-space-between">
       <div className="navigation__wrapper">
         <button type="button" className="logo-btn" onClick={() => { history.push('/'); }}>
-          <Uik.ReefLogo className="navigation__logo" />
+          { mainnetSelected ? <Uik.ReefLogo className="navigation__logo" /> : <Uik.ReefTestnetLogo className="navigation__logo" /> }
         </button>
 
         {display && (
