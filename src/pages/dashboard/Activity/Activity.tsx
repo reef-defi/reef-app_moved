055806bd4d96fd77700749b66a7d6cffb5128023
matjazonline <matjazonline@gmail.com>
2023-02-09 14:20:13 +0100

    nfts, activity integration

---
+++ src/pages/dashboard/Activity/Activity.tsx
@@ -0,0 +1,66 @@
+import {
+  appState, createEmptyTokenWithAmount, hooks, ReefSigner, Network,
+} from '@reef-defi/react-lib';
+import Uik from '@reef-defi/ui-kit';
+import React from 'react';
+import './activity.css';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import ActivityItem, { Skeleton } from './ActivityItem';
+
+const noActivityTokenDisplay = createEmptyTokenWithAmount();
+noActivityTokenDisplay.address = '0x';
+noActivityTokenDisplay.iconUrl = '';
+noActivityTokenDisplay.name = 'No account history yet.';
+
+export const Activity = (): JSX.Element => {
+  const transfers = hooks.useObservableState(appState.transferHistory$);
+
+  const signer: ReefSigner | undefined |null = hooks.useObservableState(appState.selectedSigner$);
+  const network: Network|undefined = hooks.useObservableState(appState.currentNetwork$);
+
+  return (
+    <div className="token-activity activity">
+      <div className="activity__head">
+        <Uik.Text type="title" text="Activity" className="activity__title" />
+        {
+          !!signer?.address && !!network?.reefscanUrl
+          && (
+          <Uik.Button
+            size="small"
+            icon={faArrowUpRightFromSquare}
+            text="Open Explorer"
+            onClick={() => window.open(`${network?.reefscanUrl}/account/${signer.address}`)}
+          />
+          )
+        }
+      </div>
+
+      <div className={`col-12 card  ${transfers?.length ? 'card-bg-light' : ''}`}>
+        {!!transfers && !transfers.length && <div className="no-token-activity">No recent transfer activity.</div>}
+        {!!transfers && !!transfers.length && (
+        <div>
+            {transfers.map((item, index) => (
+              <ActivityItem
+                // eslint-disable-next-line
+                key={index}
+                timestamp={item.timestamp}
+                token={item.token}
+                url={item.url}
+                inbound={item.inbound}
+              />
+            ))}
+        </div>
+        )}
+        {!transfers && (
+        <>
+          <Skeleton />
+          <Skeleton />
+          <Skeleton />
+          <Skeleton />
+          <Skeleton />
+        </>
+        ) }
+      </div>
+    </div>
+  );
+};
